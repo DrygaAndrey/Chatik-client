607be977c3e227842cced3a4976e7629be8946c7
dryga andrey <driga.and@gmail.com>
2022-10-17 18:43:12 +0300

    client commit

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,6 @@
 import io from "socket.io-client";
-const socket = io('https://fierce-brushlands-23055.herokuapp.com/');
+const socket = io('https://react-chatek-server.herokuapp.com/');
+
 //const socket = io('localhost:80');
 console.log('socket created:',socket);
 export default socket;
